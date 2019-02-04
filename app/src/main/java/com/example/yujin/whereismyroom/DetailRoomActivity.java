99ca50ee574fbc8caa83b68b07b1aee988142e31
yujinK <yujinkim.dev@gmail.com>
2019-02-05 04:09:59 +0900

    feat: activity_detail_room.xml 개발중

--- app/src/main/java/com/example/yujin/whereismyroom/DetailRoomActivity.java
+++ app/src/main/java/com/example/yujin/whereismyroom/DetailRoomActivity.java
@@ -4,7 +4,11 @@ import android.databinding.DataBindingUtil;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
 import com.example.yujin.whereismyroom.databinding.ActivityDetailRoomBinding;
 
@@ -24,7 +28,7 @@ public class DetailRoomActivity extends AppCompatActivity {
         setSupportActionBar(binding.detailToolbar.toolbar);
         getSupportActionBar().setTitle(getString(R.string.detailToolBar)); //TODO: ToolBar 뭘로 표시할 지 생각해보기
 
-        //툴바 왼쪽 back 버튼
+        //ToolBar 왼쪽 back 버튼
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setHomeAsUpIndicator(R.drawable.baseline_arrow_back_white_24);
 
@@ -35,5 +39,24 @@ public class DetailRoomActivity extends AppCompatActivity {
                 finish();
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        //ToolBar 오른쪽 edit 버튼
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.detail_edit_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_edit:
+                Toast.makeText(getApplicationContext(), "Select Edit", Toast.LENGTH_SHORT).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
